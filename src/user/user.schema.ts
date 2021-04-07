2c061972d582f457e1f984b7416613bc03c68e15
experts-web2 <experts.web2@gmail.com>
2021-04-07 21:00:51 +0500

    Created User Schema, User Module, User Service. Connected to MongoDB

---
+++ src/user/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ unique: true, required: true })
+  phone: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, type: MongooseSchema.Types.ObjectId, ref: 'City' })
+  city: string;
+
+  @Prop({ required: true, type: MongooseSchema.Types.ObjectId, ref: 'Center' })
+  center: string;
+
+  @Prop({ default: false })
+  verified: boolean;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
